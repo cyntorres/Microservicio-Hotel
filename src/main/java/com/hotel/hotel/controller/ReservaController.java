ed25e334148302e59aeb20c28c5457c82dba77d6
cynthia <cyn.torres.leal@gmail.com>
2026-03-23 19:46:28 -0300

    Microservicio Hotel con Spring Boot y Oracle

--- src/main/java/com/hotel/hotel/controller/ReservaController.java
+++ src/main/java/com/hotel/hotel/controller/ReservaController.java
@@ -13,7 +13,6 @@ import com.hotel.hotel.service.ReservaService;
 // IMPORTACIONES SPRING
 // ===============================
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 // ===============================
